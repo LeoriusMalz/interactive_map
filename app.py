f519df533ebdfc8d84f73c6dad0c7fa1614021c7
LeoriusMalz <leorius.malz@gmail.com>
2025-08-23 17:40:07 +0300

    Updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template  
   
 app = Flask(__name__, template_folder='templates')  
@@ -7,4 +8,15 @@ def web():
     return render_template('index.html')  
   
 if __name__ == "__main__":  
+=======
+from flask import Flask, render_template  
+  
+app = Flask(__name__, template_folder='templates')  
+  
+@app.route("/")  
+def web():  
+    return render_template('index.html')
+  
+if __name__ == "__main__":  
+>>>>>>> 15e51ec (Added all files)
     app.run(debug=True, host="0.0.0.0", port='80')
