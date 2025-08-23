637a61d868707ecbcd45dc8bb0f2761e708db65d
LeoriusMalz <leorius.malz@gmail.com>
2025-08-23 17:35:20 +0300

    Updated markers and added the opportunity to add markers

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
 =======
 from flask import Flask, render_template  
   
-app = Flask(__name__, template_folder='templates')  
+app = Flask(__name__, template_folder='templates')
+
+
   
 @app.route("/")  
 def web():  
